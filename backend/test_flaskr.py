7cdb14866ab59cb4cc7e61a7889c6214ee7ab01c
martin-creator <martinlubowa@outlook.com>
2022-06-24 13:26:54 +0300

    First TDD tests

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -15,7 +15,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name) # Not yet done with the tests
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -33,6 +33,50 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_categories_success(self):
+        res = self.client().get("/categories")
+        self.assertEqual(res.status_code, 200)
+
+    def test_add_category_sucess(self):
+        res = self.client().post("/categories", json={"type": "Entertainment"})
+        data = json.loads(res.data)
+        category = Category.query.get(data["added"])
+
+        self.assertEqual(res.status_code, 200)
+        self.assertIsNotNone(category)
+
+    
+    def test_add_category_bad_request():
+        res = self.client().post("/categories", json={})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(data["error"], 400)
+    
+    
+    def test_get_paginated_questions(self):
+        res = self.client().get("/questions")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(len(data["questions"]))
+        self.assertTrue(data["total_questions"])
+        self.assertTrue(len(data["categories"]))
+
+    
+    def test_get_paginated_questions_not_found(self):
+        res = self.client().get("/questions?page=1000")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data["error"], 404)
+
+    
+    
+
+    
+
+
 
 
 # Make the tests conveniently executable
